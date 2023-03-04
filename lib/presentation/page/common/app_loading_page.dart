e5413f79d63a3a3ffe438faf690e3dd12efbc439
nhoaiduy <n.hoaiduy1509@gmail.com>
2023-03-04 21:33:40 +0700

    app loading

--- lib/presentation/page/common/app_loading_page.dart
+++ lib/presentation/page/common/app_loading_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:steemit/util/path/image_path.dart';
+import 'package:steemit/util/style/base_color.dart';
+import 'package:steemit/util/style/base_image.dart';
 
 class AppLoadingPage extends StatelessWidget {
   const AppLoadingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: BaseColor.green500,
+      body: Center(
+        child: BaseImage.base(ImagePath.appIcon, color: Colors.white),
+      ),
+    );
   }
 }
